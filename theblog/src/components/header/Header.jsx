477ed91f0cd46d592a8a4cfa0b2e3400ea691540
Rohit Tiwari <rohit.tiwari968@gmail.com>
2020-11-05 14:05:12 +0530

    Initial

---
+++ theblog/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <h3>Home</h3>
+      </div>
+      <div className="header__right"></div>
+    </div>
+  );
+};
+
+export default Header;
